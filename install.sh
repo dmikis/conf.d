2673e019aee5cd2eb88eb93406fc9af7a8aa4fe2
Kirill Dmitrenko <kdmitrenko@gmail.com>
2011-09-07 20:40:32 +0400

    Including bash aliases fixed. Install script added.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -f ~/.bash_profile ]; then
+    /bin/rm -v ~/.bash_profile
+fi
+
+/bin/ln -v -s ~/conf.d/bash/profile ~/.bash_profile
+
+if [ -f ~/.bash_aliases ]; then
+    /bin/rm -v ~/.bash_aliases
+fi
+
+/bin/ln -v -s ~/conf.d/bash/aliases ~/.bash_aliases
+
+if [ -f ~/.vimrc ]; then
+    /bin/rm -v ~/.vimrc
+fi
+
+/bin/ln -v -s ~/conf.d/vim/rc ~/.vimrc
